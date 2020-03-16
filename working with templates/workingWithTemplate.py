daed0df7289098b52cf39fc35ea6811d335f71d7
Praghadesh <61776225+PraghadeshManivannan@users.noreply.github.com>
2020-03-16 23:47:51 +0530

    Add files via upload

---
+++ working with templates/workingWithTemplate.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
+
+
+
+
+@app.route("/")
+def homeMain():
+    return render_template('layout.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
